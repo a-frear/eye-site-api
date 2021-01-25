cbad12fe0071d4c47458cad10bc0697f97824f47
a_frear <amy.frear@gmail.com>
2021-01-25 13:39:36 -0500

    Initial Commit

--- src/app.js
+++ src/app.js
@@ -2,18 +2,102 @@ require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
+const {CLIENT_ORIGIN} = require('./config')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
+const path = require("path")
+const expressSession = require("express-session")
+const passport = require("passport")
+const Auth0Strategy = require("passport-auth0")
+const authRouter = require("./auth")
 
+/**
+ * App Variables
+ */
 const app = express()
 
 const morganOption = (NODE_ENV === 'production')
   ? 'tiny'
   : 'common';
 
+
+
 app.use(morgan(morganOption))
 app.use(helmet())
-app.use(cors())
+app.use(
+  cors({
+      origin: CLIENT_ORIGIN
+  })
+)
+
+/**
+ * Session Configuration
+ */
+
+/*const session = {
+  secret: process.env.SESSION_SECRET,
+  cookie: {},
+  resave: false,
+  saveUninitialized: false
+};
+
+if (app.get("env") === "production") {
+  // Serve secure cookies, requires HTTPS
+  session.cookie.secure = true;
+}
+
+/**
+ *  App Configuration
+ */
+
+/*app.use(express.static(path.join(__dirname, "public")));
+
+app.use(expressSession(session));
+
+passport.use(strategy);
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
+
+// Creating custom middleware with Express
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.isAuthenticated();
+  next();
+});
+
+// Router mounting
+app.use("/", authRouter);
+
+/**
+ * Passport Configuration
+ */
+
+// const strategy = new Auth0Strategy(
+//   {
+//     domain: process.env.AUTH0_DOMAIN,
+//     clientID: process.env.AUTH0_CLIENT_ID,
+//     clientSecret: process.env.AUTH0_CLIENT_SECRET,
+//     callbackURL: process.env.AUTH0_CALLBACK_URL
+//   },
+//   function(accessToken, refreshToken, extraParams, profile, done) {
+//     /**
+//      * Access tokens are used to authorize users to an API
+//      * (resource server)
+//      * accessToken is the token to call the Auth0 API
+//      * or a secured third-party API
+//      * extraParams.id_token has the JSON Web Token
+//      * profile has all the information from the user
+//      */
+//     return done(null, profile);
+//   }
+// );
 
 app.get('/', (req,res) => {
     res.send('Hello, world!')
