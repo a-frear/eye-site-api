24a46e9486308a97189692a3f3f86d5e1b46fae6
a_frear <amy.frear@gmail.com>
2021-01-31 16:51:26 -0500

    Update comments-endpoints.spec.js

--- test/comments-endpoints.spec.js
+++ test/comments-endpoints.spec.js
@@ -78,52 +78,52 @@ describe('Comments Endpoints', function() {
           })
       })
     
-        // describe(`POST /api/comments`, () => {
-        //   const testVideos = makeVideosArray()
+        describe(`POST /api/comments`, () => {
+          const testVideos = makeVideosArray()
      
-        //   beforeEach('insert videos', () => {
-        //       return db
-        //           .into('videos')
-        //           .insert(testVideos)
-        //   })
+          beforeEach('insert videos', () => {
+              return db
+                  .into('videos')
+                  .insert(testVideos)
+          })
             
-        //     it(`creates a comment and responds with 201 and the new comment`, () => {
-        //         this.retries(3)
-        //         const newComment = {
-        //           content: 'Test new comment content...',
-        //           user_name: 'test_user_123',
-        //           video_id: 1
-        //         }
-        //         return supertest(app)
-        //             .post(`/api/comments`)
-        //             .send(newComment)
-        //             // .expect(201)
-        //             .expect(res => {
-        //                 expect(res.body.content).to.eql(newComment.content)
-        //                 expect(res.body.video_id).to.eql(newComment.video_id)
-        //                 expect(res.body.user_name).to.eql(newComment.user_name)
-        //                 expect(res.body).to.have.property('id')
-        //                 expect(res.headers.location).to.eql(`/api/comments/${res.body.id}`)
-        //                 const expected = new Date().toLocaleString()
-        //                 const actual = new Date(res.body.modified).toLocaleString()
-        //                 expect(actual).to.eql(expected)
-        //             })
-        //             .then(postRes => 
-        //                 supertest(app)
-        //                 .get(`/api/comments/${postRes.body.id}`)
-        //                 .expect(postRes.body)
-        //                 )
-        //     })
+            it(`creates a comment and responds with 201 and the new comment`, () => {
+                this.retries(3)
+                const newComment = {
+                  content: 'Test new comment content...',
+                  user_name: 'test_user_123',
+                  video_id: 1
+                }
+                return supertest(app)
+                    .post(`/api/comments`)
+                    .send(newComment)
+                    // .expect(201)
+                    .expect(res => {
+                        expect(res.body.content).to.eql(newComment.content)
+                        expect(res.body.video_id).to.eql(newComment.video_id)
+                        expect(res.body.user_name).to.eql(newComment.user_name)
+                        expect(res.body).to.have.property('id')
+                        expect(res.headers.location).to.eql(`/api/comments/${res.body.id}`)
+                        const expected = new Date().toLocaleString()
+                        const actual = new Date(res.body.modified).toLocaleString()
+                        expect(actual).to.eql(expected)
+                    })
+                    .then(postRes => 
+                        supertest(app)
+                        .get(`/api/comments/${postRes.body.id}`)
+                        .expect(postRes.body)
+                        )
+            })
 
-        //     it('removes XSS attack content from response', () => {
-        //         const { maliciousComment, expectedComment } = makeMaliciousComment()
-        //         return supertest(app)
-        //           .post(`/api/comments`)
-        //           .send(maliciousComment)
-        //         //   .expect(201)
-        //           .expect(res => {
-        //             expect(res.body.content).to.eql(expectedComment.content)
-        //           })
-        //       })
-        // })
+            it('removes XSS attack content from response', () => {
+                const { maliciousComment, expectedComment } = makeMaliciousComment()
+                return supertest(app)
+                  .post(`/api/comments`)
+                  .send(maliciousComment)
+                //   .expect(201)
+                  .expect(res => {
+                    expect(res.body.content).to.eql(expectedComment.content)
+                  })
+              })
+        })
     })
