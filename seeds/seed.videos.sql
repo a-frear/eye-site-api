87bfc80e0d101fb70121448f48fd12c93b14489a
a_frear <amy.frear@gmail.com>
2021-01-31 15:01:43 -0500

    Update seed.videos.sql

--- seeds/seed.videos.sql
+++ seeds/seed.videos.sql
@@ -1,20 +1,20 @@
 INSERT INTO videos (title, author, url, eye, pink_eye)
 VALUES
-    ('eye love you', 'Amy Frear', 'https://www.youtube.com/embed/nzs-BK9gAuU?rel=0', 'http://localhost:8000/images/asset-1.png', 'http://localhost:8000/images/pink-eye-1.png'), 
-    ('Eye Date', 'Iva Yos', 'https://www.youtube.com/embed/c3DnVtFcu6g?rel=0', 'http://localhost:8000/images/asset-2.png', 'http://localhost:8000/images/pink-eye-2.png'),
-    ('Wink or Blink', 'Matthew Ober', 'https://www.youtube.com/embed/z3jTlVILgLw?rel=0', 'http://localhost:8000/images/asset-3.png', 'http://localhost:8000/images/pink-eye-3.png'),
-    ('eyeSweat', 'Amy Frear', 'https://www.youtube.com/embed/SQpEnMM_ZVk?rel=0', 'http://localhost:8000/images/asset-4.png', 'http://localhost:8000/images/pink-eye-4.png'),
-    ('EYE SITE PROMO', 'Matthew Ober', 'https://www.youtube.com/embed/dxU6G88Htkc?rel=0', 'http://localhost:8000/images/asset-5.png', 'http://localhost:8000/images/pink-eye-5.png'),
-    ('Eye Practice', 'Iva Yos', 'https://www.youtube.com/embed/zM3tcl7usag?rel=0', 'http://localhost:8000/images/asset-6.png', 'http://localhost:8000/images/pink-eye-6.png'),
-    ('behold beauty', 'Amy Frear', 'https://www.youtube.com/embed/UpG4WyUJhAc?rel=0', 'http://localhost:8000/images/asset-7.png', 'http://localhost:8000/images/pink-eye-7.png'),
-    ('Eye Site Promo', 'Iva Yos', 'https://www.youtube.com/embed/5GVBZjn_aHY?rel=0', 'http://localhost:8000/images/asset-8.png', 'http://localhost:8000/images/pink-eye-8.png'),
-    ('eye just cannot right now', 'Amy Frear', 'https://www.youtube.com/embed/42mNrSHO_nQ?rel=0', 'http://localhost:8000/images/asset-9.png', 'http://localhost:8000/images/pink-eye-9.png'),
-    ('staring contest #1', 'Amy Frear', 'https://www.youtube.com/embed/nzs-BK9gAuU?rel=0', 'http://localhost:8000/images/asset-10.png', 'http://localhost:8000/images/pink-eye-10.png'),
-    ('eye experiments', 'Matthew Ober', 'https://www.youtube.com/embed/z2cv2vXs_fs?rel=0', 'http://localhost:8000/images/asset-11.png', 'http://localhost:8000/images/pink-eye-11.png'),
-    ('do not mess with your eye', 'Amy Frear', 'https://www.youtube.com/embed/4l8WRt01N0E?rel=0', 'http://localhost:8000/images/asset-12.png', 'http://localhost:8000/images/pink-eye-12.png'),
-    ('mirror eye', 'Matthew Ober', 'https://www.youtube.com/embed/vcwg1K_eGCQ?rel=0', 'http://localhost:8000/images/asset-13.png', 'http://localhost:8000/images/pink-eye-13.png'),
-    ('eye on the ball', 'Amy Frear', 'https://www.youtube.com/embed/M78rsWdFYhE?rel=0', 'http://localhost:8000/images/asset-14.png', 'http://localhost:8000/images/pink-eye-14.png'),
-    ('wide open', 'Amy Frear', 'https://www.youtube.com/embed/pYHgutvpxWQ?rel=0', 'http://localhost:8000/images/asset-15.png', 'http://localhost:8000/images/pink-eye-15.png');
+    ('eye love you', 'Amy Frear', 'https://www.youtube.com/embed/nzs-BK9gAuU?rel=0', 'https://enigmatic-brook-32003.herokuapp.com/images/asset-1.png', 'https://enigmatic-brook-32003.herokuapp.com/images/pink-eye-1.png'), 
+    ('Eye Date', 'Iva Yos', 'https://www.youtube.com/embed/c3DnVtFcu6g?rel=0', 'https://enigmatic-brook-32003.herokuapp.com/images/asset-2.png', 'https://enigmatic-brook-32003.herokuapp.com/images/pink-eye-2.png'),
+    ('Wink or Blink', 'Matthew Ober', 'https://www.youtube.com/embed/z3jTlVILgLw?rel=0', 'https://enigmatic-brook-32003.herokuapp.com/images/asset-3.png', 'https://enigmatic-brook-32003.herokuapp.com/images/pink-eye-3.png'),
+    ('eyeSweat', 'Amy Frear', 'https://www.youtube.com/embed/SQpEnMM_ZVk?rel=0', 'https://enigmatic-brook-32003.herokuapp.com/images/asset-4.png', 'https://enigmatic-brook-32003.herokuapp.com/images/pink-eye-4.png'),
+    ('EYE SITE PROMO', 'Matthew Ober', 'https://www.youtube.com/embed/dxU6G88Htkc?rel=0', 'https://enigmatic-brook-32003.herokuapp.com/images/asset-5.png', 'https://enigmatic-brook-32003.herokuapp.com/images/pink-eye-5.png'),
+    ('Eye Practice', 'Iva Yos', 'https://www.youtube.com/embed/zM3tcl7usag?rel=0', 'https://enigmatic-brook-32003.herokuapp.com/images/asset-6.png', 'https://enigmatic-brook-32003.herokuapp.com/images/pink-eye-6.png'),
+    ('behold beauty', 'Amy Frear', 'https://www.youtube.com/embed/UpG4WyUJhAc?rel=0', 'https://enigmatic-brook-32003.herokuapp.com/images/asset-7.png', 'https://enigmatic-brook-32003.herokuapp.com/images/pink-eye-7.png'),
+    ('Eye Site Promo', 'Iva Yos', 'https://www.youtube.com/embed/5GVBZjn_aHY?rel=0', 'https://enigmatic-brook-32003.herokuapp.com/images/asset-8.png', 'https://enigmatic-brook-32003.herokuapp.com/images/pink-eye-8.png'),
+    ('eye just cannot right now', 'Amy Frear', 'https://www.youtube.com/embed/42mNrSHO_nQ?rel=0', 'https://enigmatic-brook-32003.herokuapp.com/images/asset-9.png', 'https://enigmatic-brook-32003.herokuapp.com/images/pink-eye-9.png'),
+    ('staring contest #1', 'Amy Frear', 'https://www.youtube.com/embed/nzs-BK9gAuU?rel=0', 'https://enigmatic-brook-32003.herokuapp.com/images/asset-10.png', 'https://enigmatic-brook-32003.herokuapp.com/images/pink-eye-10.png'),
+    ('eye experiments', 'Matthew Ober', 'https://www.youtube.com/embed/z2cv2vXs_fs?rel=0', 'https://enigmatic-brook-32003.herokuapp.com/images/asset-11.png', 'https://enigmatic-brook-32003.herokuapp.com/images/pink-eye-11.png'),
+    ('do not mess with your eye', 'Amy Frear', 'https://www.youtube.com/embed/4l8WRt01N0E?rel=0', 'https://enigmatic-brook-32003.herokuapp.com/images/asset-12.png', 'https://enigmatic-brook-32003.herokuapp.com/images/pink-eye-12.png'),
+    ('mirror eye', 'Matthew Ober', 'https://www.youtube.com/embed/vcwg1K_eGCQ?rel=0', 'https://enigmatic-brook-32003.herokuapp.com/images/asset-13.png', 'https://enigmatic-brook-32003.herokuapp.com/images/pink-eye-13.png'),
+    ('eye on the ball', 'Amy Frear', 'https://www.youtube.com/embed/M78rsWdFYhE?rel=0', 'https://enigmatic-brook-32003.herokuapp.com/images/asset-14.png', 'https://enigmatic-brook-32003.herokuapp.com/images/pink-eye-14.png'),
+    ('wide open', 'Amy Frear', 'https://www.youtube.com/embed/pYHgutvpxWQ?rel=0', 'https://enigmatic-brook-32003.herokuapp.com/images/asset-15.png', 'https://enigmatic-brook-32003.herokuapp.com/images/pink-eye-15.png');
 
 
 INSERT INTO comments (video_id, user_name, content)
