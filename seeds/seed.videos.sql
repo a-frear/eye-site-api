385def6107cb6a6ed1fbaf10488d7ae6875f887d
a_frear <amy.frear@gmail.com>
2021-02-13 13:53:36 -0500

    Update seed.videos.sql

--- seeds/seed.videos.sql
+++ seeds/seed.videos.sql
@@ -1,27 +1,22 @@
 INSERT INTO videos (title, author, url, eye, pink_eye)
 VALUES
-    ('eye love you', 'Amy Frear', 'https://www.youtube.com/embed/nzs-BK9gAuU?rel=0', 'https://enigmatic-brook-32003.herokuapp.com/images/asset-1.png', 'https://enigmatic-brook-32003.herokuapp.com/images/pink-eye-1.png'), 
-    ('Eye Date', 'Iva Yos', 'https://www.youtube.com/embed/c3DnVtFcu6g?rel=0', 'https://enigmatic-brook-32003.herokuapp.com/images/asset-2.png', 'https://enigmatic-brook-32003.herokuapp.com/images/pink-eye-2.png'),
-    ('Wink or Blink', 'Matthew Ober', 'https://www.youtube.com/embed/z3jTlVILgLw?rel=0', 'https://enigmatic-brook-32003.herokuapp.com/images/asset-3.png', 'https://enigmatic-brook-32003.herokuapp.com/images/pink-eye-3.png'),
-    ('eyeSweat', 'Amy Frear', 'https://www.youtube.com/embed/SQpEnMM_ZVk?rel=0', 'https://enigmatic-brook-32003.herokuapp.com/images/asset-4.png', 'https://enigmatic-brook-32003.herokuapp.com/images/pink-eye-4.png'),
-    ('EYE SITE PROMO', 'Matthew Ober', 'https://www.youtube.com/embed/dxU6G88Htkc?rel=0', 'https://enigmatic-brook-32003.herokuapp.com/images/asset-5.png', 'https://enigmatic-brook-32003.herokuapp.com/images/pink-eye-5.png'),
-    ('Eye Practice', 'Iva Yos', 'https://www.youtube.com/embed/zM3tcl7usag?rel=0', 'https://enigmatic-brook-32003.herokuapp.com/images/asset-6.png', 'https://enigmatic-brook-32003.herokuapp.com/images/pink-eye-6.png'),
-    ('behold beauty', 'Amy Frear', 'https://www.youtube.com/embed/UpG4WyUJhAc?rel=0', 'https://enigmatic-brook-32003.herokuapp.com/images/asset-7.png', 'https://enigmatic-brook-32003.herokuapp.com/images/pink-eye-7.png'),
     ('Eye Site Promo', 'Iva Yos', 'https://www.youtube.com/embed/5GVBZjn_aHY?rel=0', 'https://enigmatic-brook-32003.herokuapp.com/images/asset-8.png', 'https://enigmatic-brook-32003.herokuapp.com/images/pink-eye-8.png'),
-    ('eye just cannot right now', 'Amy Frear', 'https://www.youtube.com/embed/42mNrSHO_nQ?rel=0', 'https://enigmatic-brook-32003.herokuapp.com/images/asset-9.png', 'https://enigmatic-brook-32003.herokuapp.com/images/pink-eye-9.png'),
-    ('staring contest #1', 'Amy Frear', 'https://www.youtube.com/embed/N31Zr0Vjx5w?rel=0', 'https://enigmatic-brook-32003.herokuapp.com/images/asset-10.png', 'https://enigmatic-brook-32003.herokuapp.com/images/pink-eye-10.png'),
-    ('eye experiments', 'Matthew Ober', 'https://www.youtube.com/embed/z2cv2vXs_fs?rel=0', 'https://enigmatic-brook-32003.herokuapp.com/images/asset-11.png', 'https://enigmatic-brook-32003.herokuapp.com/images/pink-eye-11.png'),
-    ('do not mess with your eye', 'Amy Frear', 'https://www.youtube.com/embed/4l8WRt01N0E?rel=0', 'https://enigmatic-brook-32003.herokuapp.com/images/asset-12.png', 'https://enigmatic-brook-32003.herokuapp.com/images/pink-eye-12.png'),
-    ('mirror eye', 'Matthew Ober', 'https://www.youtube.com/embed/vcwg1K_eGCQ?rel=0', 'https://enigmatic-brook-32003.herokuapp.com/images/asset-13.png', 'https://enigmatic-brook-32003.herokuapp.com/images/pink-eye-13.png'),
-    ('eye on the ball', 'Amy Frear', 'https://www.youtube.com/embed/M78rsWdFYhE?rel=0', 'https://enigmatic-brook-32003.herokuapp.com/images/asset-14.png', 'https://enigmatic-brook-32003.herokuapp.com/images/pink-eye-14.png'),
-    ('wide open', 'Amy Frear', 'https://www.youtube.com/embed/pYHgutvpxWQ?rel=0', 'https://enigmatic-brook-32003.herokuapp.com/images/asset-15.png', 'https://enigmatic-brook-32003.herokuapp.com/images/pink-eye-15.png');
+    ('Human Tears Under a Microscope', 'You Tube', 'https://www.youtube.com/embed/C51FogqiaBU?rel=0', 'https://enigmatic-brook-32003.herokuapp.com/images/asset-3.png', 'https://enigmatic-brook-32003.herokuapp.com/images/pink-eye-3.png'),
+    ('eyeSweat', 'Amy Frear', 'https://www.youtube.com/embed/SQpEnMM_ZVk?rel=0', 'https://enigmatic-brook-32003.herokuapp.com/images/asset-4.png', 'https://enigmatic-brook-32003.herokuapp.com/images/pink-eye-4.png'),
+    ('How Animals and People See the World Differently', 'National Geographic', 'https://www.youtube.com/embed/6HWVgQdSTDQ?rel=0', 'https://enigmatic-brook-32003.herokuapp.com/images/asset-13.png', 'https://enigmatic-brook-32003.herokuapp.com/images/pink-eye-13.png'),
+    ('Eyes without a face', 'Billy Idol', 'https://www.youtube.com/embed/9OFpfTd0EIs?rel=0', 'https://enigmatic-brook-32003.herokuapp.com/images/asset-5.png', 'https://enigmatic-brook-32003.herokuapp.com/images/pink-eye-5.png'),
+    ('What happens inside your eyes', 'You Tube', 'https://www.youtube.com/watch?v=eySkNWTI03Q?rel=0', 'https://enigmatic-brook-32003.herokuapp.com/images/asset-6.png', 'https://enigmatic-brook-32003.herokuapp.com/images/pink-eye-6.png'),
+    ('Why Science needs people who cry', 'Ted Talk', 'https://www.youtube.com/embed/ZEgqJe2QxAo?rel=0', 'https://enigmatic-brook-32003.herokuapp.com/images/asset-7.png', 'https://enigmatic-brook-32003.herokuapp.com/images/pink-eye-7.png'),
+    ('10 things you didn’t know about tears', 'You Tube', 'https://www.youtube.com/embed/n4mkcOR1x6c?rel=0', 'https://enigmatic-brook-32003.herokuapp.com/images/asset-8.png', 'https://enigmatic-brook-32003.herokuapp.com/images/pink-eye-8.png'),
+    ('Daily Yoga for Eyes', 'You Tube', 'https://www.youtube.com/embed/J7KVbo1ABcc?rel=0', 'https://enigmatic-brook-32003.herokuapp.com/images/asset-9.png', 'https://enigmatic-brook-32003.herokuapp.com/images/pink-eye-9.png'),
+    ('Adorable Googly-Eyed Sea Creature Puzzles Scientists', 'National Geographic', 'https://www.youtube.com/embed/lLKaXshILBY?rel=0', 'https://enigmatic-brook-32003.herokuapp.com/images/asset-16.png', 'https://enigmatic-brook-32003.herokuapp.com/images/pink-eye-16.png')
+    ('eye love you', 'Amy Frear', 'https://www.youtube.com/embed/2z9S6GB787I?rel=0', 'https://enigmatic-brook-32003.herokuapp.com/images/asset-15.png', 'https://enigmatic-brook-32003.herokuapp.com/images/pink-eye-15.png'),
+    ('Why do we cry?', 'You Tube', 'https://www.youtube.com/embed/85aHmFUe9To?rel=0', 'https://enigmatic-brook-32003.herokuapp.com/images/asset-11.png', 'https://enigmatic-brook-32003.herokuapp.com/images/pink-eye-11.png'),
+    ('Private Eyes', 'Hall and Oates', 'https://www.youtube.com/embed/JsntlJZ9h1U?rel=0', 'https://enigmatic-brook-32003.herokuapp.com/images/asset-12.png', 'https://enigmatic-brook-32003.herokuapp.com/images/pink-eye-12.png'),
+    ('Blood Shooting Eyes', 'National Geographic', 'https://www.youtube.com/embed/xodVcgJ8bc0?rel=0', 'https://enigmatic-brook-32003.herokuapp.com/images/asset-14.png', 'https://enigmatic-brook-32003.herokuapp.com/images/pink-eye-14.png')
+    ;
 
-
-INSERT INTO comments (video_id, user_name, content)
-VALUES 
-    (1, 'eye_friend', 'Love this!'),
-    (2, 'eyelover89', 'Wow!'),
-    (3, 'eyes4ever', 'I was on the edge of my seat!'),
-    (4, 'eye.grrrl', 'Really good!'),
-    (5, 'eye_taco', 'Eyes are life!'),
-    (6, 'eyeeeeeee', 'I asked God for this and he gave it to me!');
+    INSERT INTO comments (video_id, user_name, content)
+    VALUES 
+    (1, 'eye_friend', 'Yay!')
+    ;
